7473d8e4323aacf15bef9d68a28a362aea04c844
lethanhan97 <lethanhanzzz@gmail.com>
2020-12-14 22:14:55 +0800

    added 2 sections

--- src/app/restaurants/restaurants-list-page/restaurants-list-page.component.ts
+++ src/app/restaurants/restaurants-list-page/restaurants-list-page.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { Restaurant } from '../restaurants.model';
 
 @Component({
   selector: 'app-restaurants-list-page',
   templateUrl: './restaurants-list-page.component.html',
-  styleUrls: ['./restaurants-list-page.component.scss']
+  styleUrls: ['./restaurants-list-page.component.scss'],
 })
 export class RestaurantsListPageComponent implements OnInit {
+  private restaurants: Restaurant[] = [];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
