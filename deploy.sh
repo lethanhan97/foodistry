c2f122c993e4976e6a94e7e1e3c13e60935e7891
lethanhan97 <lethanhanzzz@gmail.com>
2020-12-20 11:41:01 +0800

    deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,7 @@
 cd "../lethanhan97.github.io"
-git init
 git remote add origin https://github.com/lethanhan97/lethanhan97.github.io.git
+git add .
+git commit -m "deploy"
+git branch -M main
+git pull origin main --allow-unrelated-histories
+git push --set-upstream origin main
